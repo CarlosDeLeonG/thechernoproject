640ca214e0d5581525be80cb6f972d15fae9edbf
Carlos <Carlos@192.168.1.70>
2014-03-12 17:52:40 -0600

    Creation of the main thread of the program...

--- Rain/src/com/thecherno/Game.java
+++ Rain/src/com/thecherno/Game.java
@@ -7,25 +7,28 @@ public class Game implements Runnable {
 	public static int scale		= 3;
 
 	private Thread thread;
+	private boolean running = false;
 	
 	public synchronized void start() {
+		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
 	
 	public synchronized void stop() {
+		running = false;
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (running) {
+			
+		}
 	}
 	
 }
