df2fae659acfb4a4379f9dba3865c7cff32b77ef
Carlos <Carlos@192.168.1.70>
2014-03-12 17:42:55 -0600

    introduction of threads...

--- Rain/src/com/thecherno/Game.java
+++ Rain/src/com/thecherno/Game.java
@@ -1,5 +1,31 @@
 package com.thecherno;
 
-public class Game {
+public class Game implements Runnable {
 
+	public static int width 	= 300;
+	public static int height 	= width / 16 * 9;
+	public static int scale		= 3;
+
+	private Thread thread;
+	
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
