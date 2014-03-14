f0f07228f158e3306ca736df163469022f5df898
Carlos <Carlos@192.168.1.70>
2014-03-13 22:26:30 -0600

    Counting and displaying the frames...

--- Rain/src/com/thecherno/rain/Game.java
+++ Rain/src/com/thecherno/rain/Game.java
@@ -21,6 +21,7 @@ public class Game extends Canvas implements Runnable {
 	public static int width 	= 300;
 	public static int height 	= width / 16 * 9;
 	public static int scale		= 3;
+	public static String title = "Rain";
 
 	private Thread thread;
 	private JFrame frame;
@@ -56,28 +57,33 @@ public class Game extends Canvas implements Runnable {
 
 	@Override
 	public void run() {
-		// How may times we render per second...
+		
 		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
 		final double ns = 1000000000.0 / 60.0;
 		double delta = 0;
-		boolean c = false;
+		int frames = 0;
+		int updates = 0;
 		while (running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
-			if (c) {
-				System.out.println("Time taken: " + (now - lastTime));
-				System.exit(0);
-			}
+			
 			lastTime = now;
 			while (delta >= 1) {
 				update();
+				updates++;
 				delta--;
 				
 			}
-			//System.out.println(now - lastTime);
-			//update();
 			render();
-			c = true;
+			frames++;
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				//System.out.println(updates + " ups, " + frames + " fps");
+				frame.setTitle(title + " | " +  updates + " ups, " + frames + " fps");
+				updates = 0;
+				frames = 0;
+			}
 		}
 		stop();
 	}
@@ -114,7 +120,7 @@ public class Game extends Canvas implements Runnable {
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
-		game.frame.setTitle("Rain");
+		game.frame.setTitle(Game.title);
 		game.frame.add(game);
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
